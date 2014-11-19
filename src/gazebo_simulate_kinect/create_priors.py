fixed priors computation

src/gazebo_simulate_kinect/create_priors.py
@@ -2,38 +2,44 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage
+import os
 
-dataset = h5py.File("out.h5")
+dataset = h5py.File(os.path.expanduser('~/grasp_deep_learning/data/unprocessed_training_data/gdl_barrett.h5'))
+priors_dataset = h5py.File('priors.h5')
 
-NUM_GRASPS = dataset['labels'].shape[0]
-NUM_HEATMAPS = dataset['labels'].shape[1]
+NUM_GRASP_TYPES = 4*4*4*4
+NUM_VC = dataset['uvd'].shape[1]
+
 X_DIM = 480
 Y_DIM = 640
 
-priors = np.zeros((NUM_HEATMAPS, NUM_HEATMAPS, X_DIM, Y_DIM))
+priors_dataset.create_dataset('priors',
+                              (NUM_GRASP_TYPES, NUM_VC, NUM_VC, X_DIM, Y_DIM),
+                              chunks=(1, NUM_VC, NUM_VC, X_DIM, Y_DIM))
 
-for index in range(NUM_GRASPS):
+priors = priors_dataset['priors']
+
+print "Building Priors"
+for index in range(NUM_GRASP_TYPES):
     print index
-    for i in range(NUM_HEATMAPS):
-        for j in range(NUM_HEATMAPS):
-            vc_0 = dataset['labels'][index][i]
-            vc_1 = dataset['labels'][index][j]
 
-            arg_max0 = np.argmin(vc_0)
-            arg_max1 = np.argmin(vc_1)
+    uvds = dataset['uvd'][index]
+    for i in range(NUM_VC):
+        for j in range(NUM_VC):
 
-            x0, y0 = arg_max0 / vc_0.shape[1], arg_max0 % vc_0.shape[1]
-            x1, y1 = arg_max1 / vc_1.shape[1], arg_max1 % vc_1.shape[1]
+            u0, v0, d0 = uvds[i]
+            u1, v1, d1 = uvds[j]
 
-            offset_x = x1-x0
-            offset_y = y1-y0
+            offset_u = u1-u0
+            offset_v = v1-v0
 
-            priors[i, j, X_DIM/2.0+offset_x, Y_DIM/2.0+offset_y] += 1
+            priors[index, i, j, X_DIM/2.0+offset_u, Y_DIM/2.0+offset_v] += 1
 
-
-for i in range(NUM_HEATMAPS):
-    for j in range(NUM_HEATMAPS):
-        priors[i, j] = scipy.ndimage.gaussian_filter(priors[i,j], sigma=3)
+print "Blurring Priors"
+for index in range(NUM_GRASP_TYPES):
+    for i in range(NUM_VC):
+        for j in range(NUM_VC):
+            priors[index, i, j] = scipy.ndimage.gaussian_filter(priors[index, i, j], sigma=3)
 
 
 import IPython
cd6db858e21e1987061132057a39f6452e54e45d
jvarley <jakevarley@gmail.com>
2015-02-10 14:21:00 -0500

    updating to use grasp_dataset

--- src/aggregate_graspit_output/main.py
+++ src/aggregate_graspit_output/main.py
@@ -7,3 +7,15 @@ DATASET_PATH = rospack.get_path('grasp_dataset')
 
 
 grasp_dataset = GraspDataset()
+
+grasp = {}
+
+for each grasps.txt
+
+
+    grasp = {}
+
+    grasp_datasets.add_grasp(grasp)
+
+
+

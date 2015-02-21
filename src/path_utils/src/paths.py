ddba68fcb941d79ba6af8359025587c07a1853ac
jvarley <jakevarley@gmail.com>
2015-02-21 15:08:10 -0500

    fixed paths

--- src/path_utils/src/paths.py
+++ src/path_utils/src/paths.py
@@ -6,12 +6,14 @@ rospack = rospkg.RosPack()
 
 DATASET_TEMPLATE_PATH = rospack.get_path('grasp_dataset')
 
-#DIR_PREFIX = "~/grasp_deep_learning/data/grasp_datasets/"
-DIR_PREFIX = "/media/Elements/gdl_data/grasp_datasets/"
+DIR_PREFIX_LOCAL = "~/grasp_deep_learning/data/grasp_datasets/"
+DIR_PREFIX_ELEMENTS = "/media/Elements/gdl_data/grasp_datasets/"
+
+DIR_PREFIX = DIR_PREFIX_ELEMENTS
 
 RAW_GRASPIT_DIR = os.path.expanduser(DIR_PREFIX + "0_raw_graspit/")
 AGG_GRASPIT_DIR = os.path.expanduser(DIR_PREFIX + "1_agg_graspit/")
 RAW_GAZEBO_DIR = os.path.expanduser(DIR_PREFIX + "2_raw_gazebo/")
 CONDENSED_GAZEBO_DIR = os.path.expanduser(DIR_PREFIX + "3_condensed_gazebo/")
 RAW_PYLEARN_DIR = os.path.expanduser(DIR_PREFIX + "4_raw_pylearn/")
-GRASP_PRIORS_DIR = os.path.expanduser(DIR_PREFIX + "5_grasp_priors/")
+GRASP_PRIORS_DIR = os.path.expanduser(DIR_PREFIX_LOCAL + "5_grasp_priors/")
